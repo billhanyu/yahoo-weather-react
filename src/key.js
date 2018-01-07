6e158cb25dd7f40a59676c47bd188a6ce865510c
Bill Yu <han.yu@duke.edu>
2018-01-07 12:55:06 +0800

    get rid of the stupid google lint rule

--- src/key.js
+++
@@ -1,4 +0,0 @@
-export default {
-  key: 'dj0yJmk9aWpDTUQ0Q01sajNxJmQ9WVdrOVVuRmxSMkp4TjJzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wYQ--',
-  secret: 'dj0yJmk9aWpDTUQ0Q01sajNxJmQ9WVdrOVVuRmxSMkp4TjJzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wYQ--',
-};
